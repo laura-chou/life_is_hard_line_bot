165353c28bbcc48fe26564768edfcf6a5ad7ad37
LauraChou <chou522128@gmail.com>
2025-10-17 02:22:15 +0800

    [feat] Add new data to json file and update message transmission format

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# life_is_hard_line_bot
+# 人生好難 Bot
+<img src="icon.png" width = "30%" alt="icon"/>
+
+#### 「人生好難 Bot」是一個每天會推播一則語錄、小知識的 LINE 機器人。
+
+#### 點擊加入好友
+<a href="https://lin.ee/LaH3eAe"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/zh-Hant.png" alt="加入好友" height="36" border="0"></a>
--- index.js
+++ index.js
@@ -11,8 +11,9 @@ const __dirname = path.dirname(__filename);
 const quotesPath = path.join(__dirname, "json", "quotes.json");
 
 let quotes = JSON.parse(fs.readFileSync(quotesPath, "utf8"));
+
 if (quotes.length === 0) {
-  console.log("❌ 語錄已全部使用完畢");
+  console.log("❌ 已全部使用完畢");
   process.exit(0);
 }
 
@@ -23,17 +24,44 @@ quotes.splice(index, 1);
 
 fs.writeFileSync(quotesPath, JSON.stringify(quotes, null, 2), "utf8");
 
-async function broadcastMessage(message) {
+async function broadcastMessage(data) {
   try {
     await axios.post("https://api.line.me/v2/bot/message/broadcast", {
-      messages: [{ type: "text", text: message }]
+      messages: [{
+        "type": "flex",
+        "altText": data.altText,
+        "contents": {
+          "type": "bubble",
+          "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "text",
+                "text": data.quote,
+                "wrap": true,
+                "color": "#000000",
+                "size": "lg",
+                "align": "start"
+              },
+              {
+                "type": "text",
+                "text": `#${data.tag}`,
+                "color": "#808080",
+                "size": "md",
+                "align": "end"
+              }
+            ]
+          }
+        }
+      }]
     }, {
       headers: {
         "Authorization": `Bearer ${process.env.CHANNEL_ACCESS_TOKEN}`,
         "Content-Type": "application/json"
       }
     });
-    console.log("✅ 廣播成功：", message);
+    console.log("✅ 廣播成功：", data);
   } catch (error) {
     console.error("❌ 廣播失敗：", error.response?.data || error.message);
   }
